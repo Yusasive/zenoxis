a7df9997ae408f32730bec97ba492f845391536b
Yusasive <yusufabdullahi2910@gmail.com>
2023-04-15 23:20:46 +0100

    new files added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './header/Header';
 import SubHeader from './subheader/SubHeader';
-import Features from './features/Features';
+
 import Utilities from './utilities/Utilities';
 import Who from './who/Who'
 import Generative from './generative/Generative';
@@ -12,7 +12,7 @@ const Home = () => {
      <Header />
       <SubHeader />
        <Utilities />
-        <Features />
+       
         <Generative />
          <Who />
     </div>
