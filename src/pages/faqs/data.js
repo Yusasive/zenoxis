afb7054e7efed8bbfd7f60694a81f83b0117b41b
yusufabdullahi2910@gmail.com <yusufabdullahi2910@gmail.com>
2023-04-20 23:41:07 +0100

    faqs added

--- src/pages/faqs/data.js
+++ src/pages/faqs/data.js
@@ -50,11 +50,5 @@ export const data = [
     suscipit commodi eum enim atque at? Et perspiciatis dolore iure
     voluptatem.`
   },
-   {
-    title: 'What is an NFT?',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-    laborum cupiditate possimus labore, hic temporibus velit dicta earum
-    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-    voluptatem.`
-  },
+   
 ];
