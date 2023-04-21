683d120b3602c4ae26d2f9bc82d9bbc923b789b1
yusufabdullahi2910@gmail.com <yusufabdullahi2910@gmail.com>
2023-04-21 17:50:41 +0100

    file updated

--- src/pages/faqs/data.js
+++ src/pages/faqs/data.js
@@ -26,29 +26,13 @@ export const data = [
     content: `You can share it, gift it or place it for sale directly from the wallet on to the markteplace. There you can see what your NFT is worth.`
   },
   {
-    title: 'What is an NFT?',
+    title: 'Can i get more detailed information',
     content: `YES! On our social media pages, you can ask more questions and get more answers OR you can send us your question as a mail.`
   },
   {
-    title: 'What is an NFT?',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-    laborum cupiditate possimus labore, hic temporibus velit dicta earum
-    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-    voluptatem.`
+    title: 'How do I mint my NFT?',
+    content: `1. Connect your wallet.
+    2. Create your collection`
   },
-  {
-    title: 'What is an NFT?',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-    laborum cupiditate possimus labore, hic temporibus velit dicta earum
-    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-    voluptatem.`
-  },
-  {
-    title: 'What is an NFT?',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-    laborum cupiditate possimus labore, hic temporibus velit dicta earum
-    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-    voluptatem.`
-  },
-   
+  
 ];
--- src/pages/home/subheader/SubHeader.js
+++ src/pages/home/subheader/SubHeader.js
@@ -18,7 +18,7 @@ identity with them wherever they go.</h1>
       <h2 className="gradient__text">What is Zenoxis ?</h2>
       <p> Zenoxis offers a unique and easy way for individuals to express themselves and connect with others in the digital world through NFTs. Each NFT avatar is a one-of-a-kind digital asset that reflects the personality and preferences of its owner according to what chooses.</p>
       <p>Additionally, our avatars are peer-to-peer (P2P), allowing users to transact directly with one another without the aid of middlemen or marketplaces.</p>
-      <Link className='Learn' to="../../About.js">Learn More Zenoxis</Link>
+      <Link className='Learn' to="/About-Us">Learn More Zenoxis</Link>
 
     </div>
   </div> 
